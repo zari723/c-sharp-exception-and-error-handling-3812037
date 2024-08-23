0e890517d305da25b2d070c608c5544cab3ae18e
zari723 <anna.nowak1@relativity.com>
2024-08-23 07:31:47 +0000

    bleb

--- src/Start/Ch3/Challenge/Program.cs
+++ src/Start/Ch3/Challenge/Program.cs
@@ -34,6 +34,7 @@ Counter.ListBadCoins();
 // =============================
 public class CoinCounter
 {
+    private List<string> _badCoins = new();
     private decimal _totalAmount = 0;
 
     // valid coin types are:
@@ -42,10 +43,37 @@ public class CoinCounter
 
     public void CountCoin(string CoinType)
     {
+        try{
+            _totalAmount+= CoinValue(CoinType);
+        }
+        catch(CoinException e){
+            _badCoins.Add(e.ObjType);
+        }
+    }
+
+    private decimal CoinValue(string CoinType){
+        switch(CoinType){
+            case "Penny":
+                return 0.01m;
+            case "Nickel":
+                return 0.05m;
+            case "Dime":
+                return 0.1m;
+            case "Quarter":
+                return 0.25m;
+            case "HalfDollar":
+                return 0.5m;
+            case "Dollar":
+                return 1.0m;
+            default:
+                throw new CoinException(CoinType, "Bad coin type");
+        }
     }
 
     public void ListBadCoins()
     {
+        Console.WriteLine($"{_badCoins.Count} bad coins counted");
+        foreach(string s in _badCoins) Console.WriteLine($"{s}");
     }
 
     public decimal Total
@@ -56,4 +84,23 @@ public class CoinCounter
 
 public class CoinException : Exception
 {
+    public string ObjType;
+
+    public CoinException() : base(){
+        ObjType = "";
+    }
+
+    public CoinException(string Message) : base(Message)
+    {
+        ObjType = "";
+    }
+
+    public CoinException(string Message, Excaption inner) : base(Message, inner)
+    {
+        ObjType = "";
+    }
+
+    public CoinException(string CoinType, string Message) : base(Message){
+        ObjType = CoinType;
+    }
 }
